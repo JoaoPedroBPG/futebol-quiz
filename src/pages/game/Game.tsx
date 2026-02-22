1e9f9308134dff87b7d127fb49ac5f8c04b28d81
JoaoPedroBPG <joaopedrogunthen@gmail.com>
2026-02-22 19:50:36 -0300

    adding shadcn

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,14 +1,30 @@
 import { CHALLENGES } from "../../data/players";
 import { CareerRow } from "./CareerRow";
-
+import { useMemo, useState } from "react";
 import "./Game.css";
-import { PlayerImage } from "./playerImage";
+import { PlayerImage } from "./PlayerImage";
 
 export function Game() {
   const challenge = CHALLENGES[0];
 
-  const revealedCount = 2;
-  const revealedClubs = challenge.career.slice(0, revealedCount);
+
+  const initialBlur = 25;
+  const decreaseBlur = initialBlur / challenge.career.length - 1;
+  const [revealedCount, setRevealedCount] = useState(1);
+  const [blurPx, setBlurPx] = useState(initialBlur);
+  const revealedClubs = useMemo(() => {
+    return challenge.career.slice(0, revealedCount);
+  }, [challenge.career, revealedCount]);
+
+    function handleWrong() {
+    setRevealedCount((prev) => Math.min(prev + 1, challenge.career.length));
+
+    setBlurPx((prev) => Math.max(prev - decreaseBlur, 0));
+  }
+
+  const isMaxRevealed = revealedCount >= challenge.career.length;
+
+
 
   return (
     <main className="game">
@@ -16,16 +32,29 @@ export function Game() {
 
       <div className="gameCard">
         <div className="game-content">
-          <h1>Guess the Player</h1>
+          <h1 className="gameTitle">Guess the Player</h1>
 
           <PlayerImage
             src={challenge.image}
             alt="Misterious player"
-            blurPx={25}
+            blurPx={blurPx}
             width="50%"
           />
 
           <CareerRow clubs={revealedClubs} />
+
+                    <button
+            className="wrongBtn"
+            type="button"
+            onClick={handleWrong}
+            disabled={isMaxRevealed && blurPx === 0}
+          >
+            Errei (provisório)
+          </button>
+
+          <small className="debugText">
+            revelados: {revealedCount}/{challenge.career.length} — blur: {blurPx}px
+          </small>
         </div>
       </div>
     </main>
